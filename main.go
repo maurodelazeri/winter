ee882f06a28d41fc6def81922ebb7022196be74e
Mauro Delazeri <maurodelazeri@gmail.com>
2018-11-07 21:13:25 -0500

    new stuff

--- main.go
+++ main.go
@@ -12,28 +12,24 @@ import (
 	"sync"
 	"syscall"
 
-	"github.com/maurodelazeri/lion/common"
 	"github.com/maurodelazeri/lion/mongo"
-	"github.com/maurodelazeri/lion/streaming/kafka/producer"
-	"github.com/maurodelazeri/winter/config"
-	venue "github.com/maurodelazeri/winter/venues"
-	"github.com/maurodelazeri/winter/venues/coinbase"
-	"github.com/pquerna/ffjson/ffjson"
+	venue "github.com/maurodelazeri/lion/venues"
+	"github.com/maurodelazeri/lion/venues/coinbase"
+	"github.com/maurodelazeri/lion/venues/config"
 	"github.com/sirupsen/logrus"
-	"github.com/urfave/cli"
 )
 
 // Winter contains configuration
 type Winter struct {
 	waitGroup sync.WaitGroup
-	venues    *WinterSyncMapConfig
+	venues    *SyncMapVenuesConfig
 	config    *config.Config
 }
 
-// WinterSyncMapConfig ...
-type WinterSyncMapConfig struct {
-	state map[string]venue.Winter
-	mutex *sync.Mutex
+// SyncMapVenuesConfig ...
+type SyncMapVenuesConfig struct {
+	state map[string]venue.Venues
+	mutex *sync.RWMutex
 }
 
 type appInit struct {
@@ -53,38 +49,9 @@ const banner = `
 
 var winter Winter
 
-func actionFunc(c *cli.Context) error {
-	if c.Bool("debug") {
-
-	}
-	if c.String("domain") == "" {
-		return cli.NewExitError("Need to specify --domain", 1)
-	}
-	domain := c.String("domain")
-	logrus.Info(domain)
-	return nil
-}
-
 func main() {
 
 	HandleInterrupt()
-
-	app := cli.NewApp()
-	app.Name = "Winter"
-	app.Usage = "Winter Streaming Datasets"
-	app.Action = actionFunc
-	app.Version = "1.0"
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:  "debug",
-			Usage: "run in debug mode",
-		},
-		cli.StringFlag{
-			Name:  "domain",
-			Usage: "the domain to generate the cert for",
-		},
-	}
-	app.Run(os.Args)
 
 	fmt.Println(banner)
 
@@ -102,30 +69,26 @@ func main() {
 		log.Fatal(err)
 	}
 
-	logrus.Info("Init mongo queue")
-	mongodb.InitQueueTrades()
-
 	logrus.Infof("Venues setup")
-	winter.venues = NewWinterSyncMapConfig()
+	winter.venues = NewSyncMapVenuesConfig()
 	SetupVenues()
-
-	// logrus.Infof("GRPC setup...")
-	// go SetupGrpcServer()
 
 	winter.waitGroup.Wait()
 
-	logrus.Infof("Registering winter on kafka")
-	hostname, _ := os.Hostname()
-	appRegister, _ := ffjson.Marshal(&appInit{
-		Application: "winter",
-		Hostname:    hostname,
-		Timestamp:   common.MakeTimestamp(),
-	})
-	err = kafkaproducer.PublishMessageAsync("applications", appRegister, int32(0), true)
-	if err != nil {
-		log.Fatal(err)
-	}
-	logrus.Infof("Kafka is ready")
+	mongodb.InitQueueBacktesting()
+
+	// logrus.Infof("Registering winter on kafka")
+	// hostname, _ := os.Hostname()
+	// appRegister, _ := ffjson.Marshal(&appInit{
+	// 	Application: "winter",
+	// 	Hostname:    hostname,
+	// 	Timestamp:   common.MakeTimestamp(),
+	// })
+	// err = kafkaproducer.PublishMessageAsync("applications", appRegister, int32(0), true)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// logrus.Infof("Kafka is ready")
 
 	logrus.Infof("Winter started \n")
 
@@ -138,31 +101,21 @@ func main() {
 // SetupVenues sets up the venues used by the westeros
 func SetupVenues() {
 	for x := range winter.config.Venues.Values() {
-		err := LoadVenue(x)
+		exch, err := LoadVenue(x)
 		if err != nil {
 			log.Printf("LoadVenue %s failed: %s", x.Name, err)
 			continue
 		}
+		exch.SetDefaults()
+		winter.venues.Put(x.Name, exch)
+		exch.Setup(x.Name, x)
+		exch.Start()
 	}
 }
 
-// SetupGrpcServer sets up the gRPC server
-// func SetupGrpcServer() {
-// 	lis, err := net.Listen("tcp", os.Getenv("GRPC_SERVER_PORT"))
-// 	if err != nil {
-// 		log.Fatalf("failed to listen: %v", err)
-// 	}
-// 	// Creates a new gRPC server
-// 	s := grpc.NewServer()
-// 	pb.RegisterAPIServer(s, &Winter{})
-
-// 	winter.waitGroup.Done()
-// 	s.Serve(lis)
-// }
-
 // LoadVenue loads an venue by name
-func LoadVenue(conf config.VenueConfig) error {
-	var exch venue.Winter
+func LoadVenue(conf config.VenueConfig) (venue.Venues, error) {
+	var exch venue.Venues
 	switch conf.Name {
 	// case "bitfinex":
 	// 	exch = new(bitfinex.Bitfinex)
@@ -175,19 +128,12 @@ func LoadVenue(conf config.VenueConfig) error {
 	case "COINBASEPRO":
 		exch = new(coinbase.Coinbase)
 	default:
-		return errors.New("venue " + conf.Name + " not found")
+		return exch, errors.New("venue " + conf.Name + " not found")
 	}
-
 	if exch == nil {
-		return errors.New("venue failed to load")
+		return exch, errors.New("venue failed to load")
 	}
-
-	exch.SetDefaults()
-	winter.venues.Put(conf.Name, exch)
-	exch.Setup(conf.Name, winter.config.Venues.Get(conf.Name))
-	exch.Start()
-
-	return nil
+	return exch, nil
 }
 
 // AdjustGoMaxProcs adjusts the maximum processes that the CPU can handle.
@@ -231,34 +177,41 @@ func Shutdown() {
 	os.Exit(1)
 }
 
-// NewWinterSyncMapConfig ...
-func NewWinterSyncMapConfig() *WinterSyncMapConfig {
-	s := &WinterSyncMapConfig{
-		state: make(map[string]venue.Winter),
-		mutex: &sync.Mutex{},
+// NewSyncMapVenuesConfig ...
+func NewSyncMapVenuesConfig() *SyncMapVenuesConfig {
+	s := &SyncMapVenuesConfig{
+		state: make(map[string]venue.Venues),
+		mutex: &sync.RWMutex{},
 	}
 	return s
 }
 
 // Put ...
-func (s *WinterSyncMapConfig) Put(key string, value venue.Winter) {
+func (s *SyncMapVenuesConfig) Put(key string, value venue.Venues) {
 	s.mutex.Lock()
 	s.state[key] = value
 	s.mutex.Unlock()
 }
 
 // Get ...
-func (s *WinterSyncMapConfig) Get(key string) venue.Winter {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+func (s *SyncMapVenuesConfig) Get(key string) venue.Venues {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	return s.state[key]
 }
 
+// GetAllKeys ...
+func (s *SyncMapVenuesConfig) GetAllKeys() map[string]venue.Venues {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	return s.state
+}
+
 // Values ...
-func (s *WinterSyncMapConfig) Values() chan venue.Winter {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-	values := make(chan venue.Winter, len(s.state))
+func (s *SyncMapVenuesConfig) Values() chan venue.Venues {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	values := make(chan venue.Venues, len(s.state))
 	for _, value := range s.state {
 		values <- value
 	}
